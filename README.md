04abff3b3e2e18bf4768b8bdd9763778a930a4f7
Steve <4391026+torvista@users.noreply.github.com>
2023-03-31 13:40:46 +0200

    add parsing of discrete files

--- README.md
+++ README.md
@@ -3,13 +3,22 @@ Your IDE may not index (find) the constants stored in the database, nor those in
 
 This script generates two local php files containing generated defines from 
 a) the configuration_keys/constants in the database
-b) the language constants from the arrays in .lang language files
+b) the language constants from the arrays in .lang language files and other discrete files.
 
-The script should be placed in the admin folder root of your development server and certainly never uploaded to a production site.
+## How to use the script
+The script should be copied to the admin folder of your **development** server and certainly never uploaded to a production site.
 
-The file is prefixed dev- so this file and the two output files will be ignored by the Zen Cart GitHub configuration if used in a Zen Cart repository.
+In your browser, login to the admin, then manually enter the name of the file "dev-list-constants.php" after the admin directory name in the browser address bar to run the script.
+
+This will generate the two files for your IDE to index.
+
+These three files are prefixed dev- so this file and the two output files will be ignored by the Zen Cart GitHub configuration if used in a Zen Cart repository.
 You may rename the script as you wish: the output files use the script name as a stub to keep them together.
 
 Options are available in the script to show/not show the actual values of the constants, which are not strictly required for indexing.
 
 Please report bugs in GitHub and if there is something missing, add it!
+
+### Changelog
+31/03/2023: moved file parsing to a function. Expanded function to allow parsing of files with array names not "$define".
+
--- files/ADMIN_FOLDER/dev-list_constants.php
+++ files/ADMIN_FOLDER/dev-list_constants.php
@@ -1,25 +1,33 @@
 <?php
+
 declare(strict_types=1);
 // https://github.com/torvista/zen-cart_list-configuration-constants
 // This script creates a file with a similar name AS THIS FILE (whatever you name it), listing the configuration keys so
 // an IDE can find them and prevent error inspections flagging "missing constants"
 // DO NOT PUT THIS FILE IN YOUR PRODUCTION SITE, it is for LOCAL DEVELOPMENT USE ONLY. Put it in your "admin" folder.
 // If the admin login screen appears...login!
-//for phpstorm inspections
+
+/*
+* @copyright torvista
+* @license http://www.zen-cart.com/license/2_0.txt GNU Public Licence V2.0
+* @version 31 Mar 2023
+ */
+
+// for phpstorm inspections
 /* @var queryFactory $db
- * @var $define
- * */
+*/
 $debug = false;
 $parse_db_configuration_constants = true;
-$show_constant_values = true;//default=false for security: just produces a list of constants with no values listed
+$show_constant_values = true; // default=false for security: just produces a list of constants with no values listed
 
 $parse_lang_define_arrays = true;
-$filename_separator = true; //prefix each block of constants with the source filename
-//////////////////////////////////////
-$filename_db_constants = basename(__FILE__, '.php') . '-db_constants.php';//use this file's name as a prefix for the
-// output file
-$filename_array_constants = basename(__FILE__, '.php') . '-array_constants.php';//use this file's name as a prefix for
+$filename_separator = true; // prefix each block of constants with the source filename
+
+///////////////////////////////////////////
+$filename_file_constants = basename(__FILE__, '.php') . '-file_constants.php'; // use this file's name as a prefix for
 // the output file
+$filename_db_constants = basename(__FILE__, '.php') . '-db_constants.php'; // use this file's name as a prefix for the
+// output file
 if (file_exists('includes/application_top.php')) {
     include 'includes/application_top.php';
 } else {
@@ -27,7 +35,7 @@ if (file_exists('includes/application_top.php')) {
 }
 if (!isset($db)) {
     exit('ERROR: $db not set');
-} // or die, return false, thrown exception, to keep phpstorm inspects happy!
+}
 if (!function_exists('mv_printVar')) {
     /**
      * @param $a
@@ -48,17 +56,54 @@ if (!function_exists('mv_printVar')) {
         if (!empty($name[1])) {
             echo '<strong>' . trim($name[1]) . '</strong> (' . gettype($a) . "):\n";
         }
-        //var_export($a);
         print_r($a);
         echo '</pre><br>';
     }
 }
+/**
+ * parse filename and optional array_names. If array_names is empty, assume it is a lang file and using $define as the array name.
+ */
+function parse_file($filename, $array_names = []): void
+{
+    global $debug, $fh, $filename_separator, $show_constant_values;
+    if ($debug) {
+        mv_printVar($array_names);
+    }
+
+    echo '<p>Reading file <b>' . $filename . '</b>: ';
+    include($filename);
+    if (count($array_names) === 0) {
+        $array_names[] = 'define';
+    }
+    $filename = $filename_separator ? "// filename=$filename;\n" : '';
+    fwrite($fh, $filename); // write data
+    foreach ($array_names as $array_name) {
+        $defines_list = "// array name=$$array_name\n";
+        if ($debug) {
+            echo '<br>array_name=$' . $array_name . '<br>';
+            mv_printVar($$array_name);
+        }
+        foreach ($$array_name as $k => $v) {
+            if ($show_constant_values) {
+                $v = str_replace("'", "\'", (string)$v);
+                $v = str_replace(["\r", "\n"], '', $v);
+                //   $v = str_replace("\n", "' . PHP_EOL . '", $v);
+            } else {
+                $v = '';
+            }
+            $defines_list .= "define('$k', '$v');\n";
+        }
+        fwrite($fh, $defines_list); // write data
+    }
+    echo 'constants added.</p>';
+}
+
 ?>
     <!DOCTYPE html>
     <html lang="en">
     <head>
         <style>
-           body {
+            body {
                 font-family: Verdana, sans-serif;
                 font-size: 70%;
             }
@@ -67,30 +112,30 @@ if (!function_exists('mv_printVar')) {
                 border-collapse: collapse;
             }
             th, td {
-                text-align:left;
+                text-align: left;
                 padding: 3px;
             }
         </style>
-        <title>Generate Files of Constants for an IDE</title>
+        <title>List Constants</title>
     </head>
     <body>
-    <h1>Generate Files of Constants for an IDE</h1>
+    <h1>Generate Files of all Constants</h1>
     <?php
     if ($parse_db_configuration_constants) { ?>
         <h2>Parsing of Database Configuration Constants</h2>
         <table>
             <tr>
                 <th>configuration_id</th>
-                <th>configuration_key</th><?php echo($show_constant_values ? '<th>configuration_value</th>' : ''); ?></tr>
+                <th>configuration_key</th><?= ($show_constant_values ? '<th>configuration_value</th>' : ''); ?></tr>
             <?php
-            $db_constants_query = 'SELECT configuration_id, configuration_title, configuration_key, configuration_value 
-            FROM ' . TABLE_CONFIGURATION . ' 
+            $db_constants_query = 'SELECT configuration_id, configuration_title, configuration_key, configuration_value
+            FROM ' . TABLE_CONFIGURATION . '
             WHERE configuration_key >""';//there is one with no key name
 
             $db_constants_result = $db->Execute($db_constants_query);
 
-            $constants = '<?php // generated ' . date('Y-m-d H:i:s') . "\n";
-            $constants .= '//$show_configuration_values = ' . ($show_constant_values ? 'true' : 'false') . "\n";
+            $constants = '<?php // generated ' . date('Y-m-d H:i:s') . ' (' . date_default_timezone_get() . ') ' . "\n";
+            $constants .= '// $show_configuration_values = ' . ($show_constant_values ? 'true' : 'false') . "\n";
             $count = 0;
             foreach ($db_constants_result as $row) {
                 $count++;
@@ -98,10 +143,10 @@ if (!function_exists('mv_printVar')) {
                 $constant_title = $row['configuration_title'];
                 $constant_name = $row['configuration_key'];
                 $constant_value = $row['configuration_value'];
-                
+
                 $constants .= "define('" . strtoupper($constant_name) . "', '";
-                //if a backslash is not replaced, it escapes the delimiter in the generated file and makes the rest of the file invalid.
-                //this seems not be not used now
+                // if a backslash is not replaced, it escapes the delimiter in the generated file and makes the rest of the file invalid.
+                // this seems not be not used now
                 if ($show_constant_values) {
                     if ($constant_value === '\\') {
                         $constants .= 'BACKSLASH replaced by file generator';
@@ -109,13 +154,13 @@ if (!function_exists('mv_printVar')) {
                         $constants .= $constant_value;
                     }
                 }
-                $constants .= "');\t\t\t//configuration_id=" . $constant_id .  " - $constant_title\n";
+                $constants .= "');\t\t\t// configuration_id=" . $constant_id . " - $constant_title\n";
                 ?>
 
                 <tr>
-                    <td><?php echo $constant_id; ?></td>
-                    <td><?php echo strtoupper($constant_name); ?></td>
-                    <?php echo($show_constant_values ? '<td>' . htmlentities($constant_value) . '</td>' : ''); ?>
+                    <td><?= $constant_id; ?></td>
+                    <td><?= strtoupper($constant_name); ?></td>
+                    <?= ($show_constant_values ? '<td>' . htmlentities($constant_value) . '</td>' : ''); ?>
                 </tr>
                 <?php
             }
@@ -125,30 +170,38 @@ if (!function_exists('mv_printVar')) {
             fclose($fh); // close file
             ?>
         </table>
-        <h2><?php echo $count; ?> constants found in <?php echo TABLE_CONFIGURATION; ?> table</h2>
+        <h2><?= $count; ?> constants found in <?= TABLE_CONFIGURATION; ?> table</h2>
         <?php
     } else { ?>
         <h2>Parsing of Database Configuration Constants: not enabled</h2>
-    <?php } ?>
+    <?php
+    } ?>
     <hr>
     <?php
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    if ($parse_lang_define_arrays) { ?>
-        <h2>Parsing of .lang define arrays</h2>
+    if ($parse_lang_define_arrays) {
+        $discrete_files = [];
+        // add individual files to this array as per format below, using the array names
+        $discrete_files = array_merge(
+            [DIR_FS_CATALOG . DIR_WS_INCLUDES . 'init_includes/init_non_db_settings.php' => ['site_specific_non_db_settings', 'non_db_settings']]  // admin version just includes this one
+        );
+        if ($debug) {
+            mv_printVar($discrete_files);
+        }
+        ?>
+        <h2>Parsing of .lang and other define arrays</h2>
         <?php
         $language = 'english';
-//admin
-        $paths_to_scan = [
-            //DIR_FS_ADMIN . 'extra_configures/',
-            //DIR_FS_ADMIN . 'extra_datafiles/',
+// admin
+        $paths_to_scan_lang = [
             DIR_FS_ADMIN . DIR_WS_LANGUAGES,
             DIR_FS_ADMIN . DIR_WS_LANGUAGES . $language . '/',
             DIR_FS_ADMIN . DIR_WS_LANGUAGES . $language . '/extra_definitions/',
             DIR_FS_ADMIN . DIR_WS_LANGUAGES . $language . '/modules/newsletters/',
         ];
-
-//shopfront
-        array_push($paths_to_scan,
+// shopfront
+        array_push(
+            $paths_to_scan_lang,
             DIR_FS_CATALOG_LANGUAGES,
             DIR_FS_CATALOG_LANGUAGES . $language . '/',
             DIR_FS_CATALOG_LANGUAGES . $language . '/classic/',
@@ -170,64 +223,50 @@ if (!function_exists('mv_printVar')) {
             DIR_FS_CATALOG_LANGUAGES . $language . '/responsive_classic/',
         );
         if ($debug) {
-            mv_printVar($paths_to_scan);
+            mv_printVar($paths_to_scan_lang);
         }
-        $fh = fopen($filename_array_constants, 'wb'); // open/create the file.
-        $defines_header = '<?php // generated' . date('Y-m-d H:i:s') . "\n";
-        $defines_header .= '//$show_constant_values = ' . ($show_constant_values ? 'true' : 'false') . "\n";
+        $fh = fopen($filename_file_constants, 'wb'); // open/create the file.
+        $defines_header = '<?php // generated ' . date('Y-m-d H:i:s') . ' (' . date_default_timezone_get() . ') ' . "\n";
+        $defines_header .= '// $show_constant_values = ' . ($show_constant_values ? 'true' : 'false') . "\n";
         fwrite($fh, $defines_header);
 
-        foreach ($paths_to_scan as $path_to_scan) { ?>
-            <h2>Path to scan: "<?php echo $path_to_scan; ?>"</h2>
+        foreach ($paths_to_scan_lang as $path_to_scan) { ?>
+            <h2>Path to scan: "<?= $path_to_scan; ?>"</h2>
             <?php
             $file_list = glob($path_to_scan . 'lang.*.php');
             if ($file_list === false || count($file_list) === 0) { ?>
-                <h3>No files found in: "<?php echo $path_to_scan; ?>"</h3>
+                <h3>No files found in: "<?= $path_to_scan; ?>"</h3>
                 <?php
                 continue;
             }
             if ($debug) {
                 mv_printVar($file_list);
             }
-            foreach ($file_list as $filename) {
-                $defines_list = '';
-                echo '<p>Reading file <b>' . $filename . '</b>: ';
 
-                //lang.gv_redeem.php and lang.gv_send.php reference other pre-defined constants in english.php and script chokes on that.
-                //todo
+            foreach ($file_list as $filename) {
+                // lang.gv_redeem.php and lang.gv_send.php reference other pre-defined constants in english.php and script chokes on that.
+                // todo
                 if ($filename === DIR_FS_CATALOG_LANGUAGES . $language . '/' . 'lang.gv_redeem.php'
                     || $filename === DIR_FS_CATALOG_LANGUAGES . $language . '/' . 'lang.gv_send.php'
                 ) {
-                    echo '**<u>FILE SKIPPED (see script)</u>**</p>';
+                    echo '<p style="background-color:red">FILE "' . $filename . '" SKIPPED (see script line ' . __LINE__ . ')</p>';
                     continue;
                 }
-
-                include($filename);
-                if ($debug) {
-                    mv_printVar($define);
-                }
-                $defines_list .= $filename_separator ? "//$filename;\n" : '';
-                $count = 0;
-                foreach ($define as $k => $v) {
-                    if ($show_constant_values) {
-                        $v = str_replace("'", "\'", (string)$v);
-                        $v = str_replace(["\r", "\n"], '', $v);
-                        //   $v = str_replace("\n", "' . PHP_EOL . '", $v);
-                    } else {
-                        $v = '';
-                    }
-                    $defines_list .= "define('$k', '$v');\n";
-                }
-                fwrite($fh, $defines_list); // write data
-                echo 'constants added.</p>';
+                parse_file($filename);
             }
-
+        } ?>
+        <h2>Parsing discrete files</h2>
+        <?php
+        foreach ($discrete_files as $key => $array_names) {
+            parse_file($key, $array_names);
         }
         fclose($fh); // close file
     } else { ?>
         <h2>Parsing of .lang define arrays: not enabled</h2>
         <?php
     } ?>
+    <hr>
+    <h4>Script complete</h4>
     <body>
     </html>
 <?php
